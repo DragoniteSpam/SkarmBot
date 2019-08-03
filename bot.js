03687d396d321458478b5513fde83bb159c0a5ad
Dragonite <mpeng@optimum.net>
2019-08-03 16:01:15 -0400

    pin message with upvote count works

--- bot.js
+++ bot.js
@@ -14,12 +14,9 @@ const events = discordie.Events;
 
 // we made these
 const Skarm = require("./javascript/skarm.js");
-const Bot = new (require("./javascript/skarmbot.js"))();
-const { ShantyCollection, Shanty } = require("./javascript/shanties.js");
+const SkarmBot = require("./javascript/skarmbot.js");
 const XKCD = require("./javascript/xkcd.js");
 const Constants = require("./javascript/constants.js");
-
-console.log(Constants);
 
 // i'm in?
 const token = fs.readFileSync("..\\descrution.txt").toString();
@@ -28,22 +25,21 @@ client.connect({
 	token: token
 });
 
-let shanties;
+let bot;
 
 client.Dispatcher.on(events.GATEWAY_READY, e => {
+    bot = new SkarmBot(client);
 	Skarm.log("Connected as " + client.User.username + ". Yippee!\n");
-    shanties = new ShantyCollection();
     Constants.initialize(client);
-    //shanties.load("ragnar-the-red.shanty");
     client.User.setGame({name: "drago please count my lines of spaghetti", type: 0});
 });
 
 // after GATEWAY_READY (becasue it's got to initalize so many different things) try to put all
 // of the actual event code in skarmbot.js to keep this main file clean
 client.Dispatcher.on(events.MESSAGE_DELETE, e=> {
-	Bot.OnMessageDelete(e);
+	bot.OnMessageDelete(e);
 });
 
 client.Dispatcher.on(events.MESSAGE_REACTION_ADD, e=> {
-    Bot.OnMessageReactionAdd(e);
+    bot.OnMessageReactionAdd(e);
 });
--- javascript/skarmbot.js
+++ javascript/skarmbot.js
@@ -2,10 +2,18 @@
 // actual bot code goes here, because i want to try to keep bot.js to delegating work on events
 const fs = require("fs");
 
+const { ShantyCollection, Shanty } = require("./shanties.js");
 const Skarm = require("./skarm.js");
 const Constants = require("./constants.js");
 
 class Bot {
+    constructor(client) {
+        this.client = client;
+        this.shanties = new ShantyCollection();
+        this.channelsPinUpvotes = {};
+    }
+    
+    // events
     OnMessageDelete(e) {
         var string = "";
         if (e.message != null){
@@ -26,25 +34,16 @@ class Bot {
     }
     
     OnMessageReactionAdd(e) {
-        Skarm.todo("implement this - auto-pin channels should be kept as a list somewhere instead of hard-coded");
-        if (e.emoji.name == "Upvote"){
-            if (e.message != null && e.message.channel_id == BLACKBIRD){
-                // This is horrible and i should feel horrible about writing it //gohere
-                Skarm.todo("find a better way to do this, also");
-                if (lastTenReactions.length>=10){
-                    lastTenReactions.shift();
-                }
-                lastTenReactions.push(e.message);
-                var count=0;
-                for (var i=0; i<lastTenReactions.length; i++){
-                    if (lastTenReactions[i]==e.message){
-                        count++;
-                    }
-                }
-                
-                if (count==3&&!e.message.pinned){
-                    e.message.pin();
-                    fetchPinnedSelf(e.message.channel);
+        const UPVOTE = 0x2b06;
+        const REQUIRED_UPVOTES = 1;
+        
+        if (e.message !== null && !e.message.pinned/* && this.channelsPinUpvotes[e.message.channel_id]*/ /* !== undefined && === true */) {
+            let upvotes = 0;
+            for (let i in e.message.reactions) {
+                let reaction = e.message.reactions[i];
+                if (reaction.emoji.name.charCodeAt(0) === UPVOTE && ++upvotes == REQUIRED_UPVOTES) {
+                    e.message.pin().catch(_ => { });
+                    break;
                 }
             }
         }
