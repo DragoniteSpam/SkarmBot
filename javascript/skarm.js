8971bc1d91420e11cec3b54fecaff257eb184579
Dragonite <mpeng@optimum.net>
2019-08-04 11:45:45 -0400

    Skarm.isWeekend, etc

--- javascript/skarm.js
+++ javascript/skarm.js
@@ -1,6 +1,14 @@
 "use strict";
 const fs = require("fs");
 
+const SUNDAY = 0;
+const MONDAY = 1;
+const TUESDAY = 2;
+const WEDNESDAY = 3;
+const THURSDAY = 4;
+const FRIDAY = 5;
+const SATURDAY = 6;
+
 // Static methods. Static methods everywhere.
 class Skarm {
     static log(message) {
@@ -13,6 +21,20 @@ class Skarm {
     
     static logError(err) {
         // you can do whatever you want i guess
+    }
+    
+    static isWeekend() {
+        let day = Date.now().getDay();
+        return (day == SUNDAY || day == SATURDAY);
+    }
+    
+    static isSkyrimDay() {
+        return isWeekend();
+    }
+    
+    static isPunDay() {
+        let day = Date.now().getDay();
+        return (day == MONDAY || day == TUESDAY);
     }
 }
 
