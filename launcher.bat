f2c0ad74685eebe89df7bd5b22092e35746c156b
Master9000 <Master.9000@mail.ru>
2020-02-09 16:48:56 -0500

    add checkout to bat

--- launcher.bat
+++ launcher.bat
@@ -2,5 +2,6 @@ git fetch origin producton
 git merge origin/producton
 node bot.js > output.txt
 git add *
-git commit -m "end of task git push"
+git checkout production
+git commit * -m "end of task git push"
 git push -u origin producton
