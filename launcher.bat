92036b98291d21298b708381414b668544f3b8a6
Master9000 <36687789+Master9000@users.noreply.github.com>
2020-02-09 16:53:02 -0500

    checkout

--- launcher.bat
+++ launcher.bat
@@ -1,6 +1,7 @@
 git fetch origin producton
 git merge origin/producton
+git checkout production -f
 node bot.js > output.txt
 git add *
-git commit -m "end of task git push"
+git commit * -m "end of task git push"
 git push -u origin producton
