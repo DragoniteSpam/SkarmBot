617c3bb2a9625d201a9ca4b52a3e5de98cadeefc
Master9000 <36687789+Master9000@users.noreply.github.com>
2020-02-09 16:59:39 -0500

    match case

--- launcher.bat
+++ launcher.bat
@@ -1,7 +1,11 @@
+git checkout producton
+
 git fetch origin producton
 git merge origin/producton
+
+
+
 node bot.js > output.txt
 git add *
-git checkout production
 git commit * -m "end of task git push"
 git push -u origin producton
