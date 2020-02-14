386c2ec2b5e510039ab2999effee65530248d5d9
Master9000 <Master.9000@mail.ru>
2020-02-13 23:13:13 -0500

    More specific commit messages

--- launcher.bat
+++ launcher.bat
@@ -3,7 +3,11 @@ git fetch origin master
 git merge origin/master
 git pull
 node bot.js > output.txt
-git add *
+
+git add users/*.zeal
+git commit users/*.zeal -m "User Updates"
+git add logs/*.*
+git commit logs/*.* -m "Log Updates"
 git commit * -m "end of task git push"
 git push 
 echo restarting...
