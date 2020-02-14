e36f4933321201b4d3e04c981116e545049b8b9c
Master9000 <36687789+Master9000@users.noreply.github.com>
2020-02-13 22:06:53 -0500

    master launcher now stays in master branch

--- launcher.bat
+++ launcher.bat
@@ -1,11 +1,13 @@
-git checkout producton
+git checkout master
 
-git fetch origin producton
-git merge origin/producton
-
+git fetch origin master
+git merge origin/master
+git pull
 
 
 node bot.js > output.txt
 git add *
 git commit * -m "end of task git push"
-git push -u origin producton
+git push 
+echo restarting...
+launcher.bat
