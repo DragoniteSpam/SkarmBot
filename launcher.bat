2d0ffd6f4cc16aa78caad8e4685cedd0bcd85f0b
Master9000 <36687789+Master9000@users.noreply.github.com>
2020-02-16 15:07:37 -0500

    only commit .txt's not everything

--- launcher.bat
+++ launcher.bat
@@ -10,7 +10,7 @@ git add users/*.zeal
 git commit users/*.zeal -m "User Updates"
 git add logs/*.*
 git commit logs/*.* -m "Log Updates"
-git commit * -m "end of task git push"
+git commit *.txt -m "end of task git push"
 git push 
 echo restarting...
 launcher.bat
