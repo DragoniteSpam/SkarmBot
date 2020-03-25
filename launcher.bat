c8132ee0ecf7af0df9dfb158679dfabfdf346bc9
Master9000 <Master.9000@mail.ru>
2020-03-25 12:24:13 -0400

    condition winR

--- launcher.bat
+++ launcher.bat
@@ -2,13 +2,47 @@ git checkout master
 git fetch origin master
 git merge origin/master
 git pull
+del oldOutput.txt
+ren output.txt oldOutput.txt
 node bot.js > output.txt
-IF %ERRORLEVEL% == 9 ( exit )
+set ERL=%ERRORLEVEL%
+IF %ERL% == 9 ( exit )
 git add users/*.zeal
 git commit users/*.zeal -m "User Updates"
 git add logs/*.*
 git commit logs/*.* -m "Log Updates"
 git commit *.txt -m "end of task git push"
 git push 
+IF %ERL% == 1 (
 echo restarting...
+launcher.bat
+exit)
+IF %ERL% == 2 (
+echo restarting...
+launcher.bat
+exit)
+IF %ERL% == 3 (
+echo restarting...
+launcher.bat
+exit)
+IF %ERL% == 4 (
+echo restarting...
+launcher.bat
+exit)
+IF %ERL% == 5 (
+echo restarting...
+launcher.bat
+exit)
+IF %ERL% == 6 (
+echo switching to dev...
+dev.bat
+exit)
+IF %ERL% == 7 (
+echo switching to 2...
+2.bat
+exit)
+IF %ERL% == 420 (
+winRestart.bat
+)
+echo failed to find a proper thing to reboot off of... I'll restart...
 launcher.bat
