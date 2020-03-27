8580d282023506ea2d3b59dd5c1cff70a20de96d
Dragonite <mpeng@optimum.net>
2020-03-27 15:22:05 -0400

    crypto class (untested)

---
+++ javascript/encryption.js
@@ -0,0 +1,34 @@
+"use strict";
+const fs = require("fs");
+const crypto = require("crypto-js");
+const Skarm = require("./skarm.js");
+
+class Encrypt {
+    static initialize() {
+        Encrypt.dataToken = fs.readFileSync("..\\..\\data.txt").toString();
+    }
+    
+    static read(filename, callback) {
+        fs.readFile(filename, function(err, data) {
+            if (err) return Skarm.logError(err);
+            callback(crypto.AES.decrypt(data, Encrypt.dataToken).toString(), filename);
+        });
+    }
+    
+    static write(filename, data) {
+        fs.writeFile(filename,
+            crypto.AES.encrypt(data, Encrypt.dataToken).toString(),
+            function(err) {
+                if (err) return Skarm.logError(err);
+            }
+        );
+    }
+    
+    static append(filename, data) {
+        Encrypt.read(filename, function(existing, filename) {
+            Encrypt.write(filename, existing + data);
+        });
+    }
+}
+
+module.exports = Encrypt;
