4e92f9d9728eb588ce934f6f93f713ee74d7aedc
Master9000 <=>
2020-05-25 17:36:39 -0400

    encryption tests are slowly being figured out

--- tests/encryptTests.js
+++ tests/encryptTests.js
@@ -35,9 +35,10 @@ class EncryptTest {
 	static execute(string){
 		EncryptTest.initialize();
 		
-		var i1= crypto.AES.encrypt(string,EncryptTest.dataToken,formatObj);
-		var o1= crypto.AES.decrypt(i1,EncryptTest.dataToken,formatObj);
-		if(string!=o1){
+		var i1= crypto.AES.encrypt(string,EncryptTest.dataToken).toString();
+		var o1= crypto.AES.decrypt(i1,EncryptTest.dataToken);
+		var f1= o1.toString(crypto.enc.Utf8);
+		if(string!=f1){
 			console.log("output from basic encrypt with no fs use failed:\t"+string+"\t"+o1);
 			return -1;
 		}
@@ -46,62 +47,27 @@ class EncryptTest {
 		
 		var filename="testEnc.test";
 		//write string and verify it is what is returned
-		fs.writeFileSync(filename,crypto.AES.encrypt(string, EncryptTest.dataToken).toString());
-		var red =fs.readFileSync(filename);
-		var output=crypto.AES.decrypt(red,EncryptTest.dataToken);
+		var enc2=crypto.AES.encrypt(string, EncryptTest.dataToken).toString();
+		fs.writeFileSync(filename,enc2);
+		var red =fs.readFileSync(filename).toString();
+		if(red!=enc2){
+			console.log("failed to read/write Synced");
+			return -1;
+		}
+		var out2=crypto.AES.decrypt(red,EncryptTest.dataToken);
+		//console.log(out2); //this is something very funky
+		var output= out2.toString(crypto.enc.Utf8);
 		if(string !=output){
 			console.log("failed to write encrypted "+string+" to "+filename+" . Output came out as: "+output+"__EOF");
 			return -1;
 		}
+		
+		
+		
+		
+		return 0;
 	}
 }
-
-
-
-
-
-
-var JsonFormatter = {
-  stringify: function(cipherParams) {
-    // create json object with ciphertext
-    var jsonObj = { ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64) };
-​
-    // optionally add iv or salt
-    if (cipherParams.iv) {
-      jsonObj.iv = cipherParams.iv.toString();
-    }
-​
-    if (cipherParams.salt) {
-      jsonObj.s = cipherParams.salt.toString();
-    }
-​
-    // stringify json object
-    return JSON.stringify(jsonObj);
-  },
-  parse: function(jsonStr) {
-    // parse json string
-    var jsonObj = JSON.parse(jsonStr);
-​
-    // extract ciphertext from json object, and create cipher params object
-    var cipherParams = CryptoJS.lib.CipherParams.create({
-      ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
-    });
-​
-    // optionally extract iv or salt
-​
-    if (jsonObj.iv) {
-      cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-    }
-​
-    if (jsonObj.s) {
-      cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-    }
-​
-    return cipherParams;
-  }
-};
-
-const formatObj={format:JsonFormatter};
 
 
 module.exports = EncryptTest;
