b6d2392852e69e5b9cc9ee9c63618cc057844b83
Master9000 <36687789+Master9000@users.noreply.github.com>
2020-06-07 15:06:43 -0400

    testing

--- tests/encryptTests.js
+++ tests/encryptTests.js
@@ -1,40 +1,53 @@
 "use strict";
+const fs = require("fs");
+const crypto = require("crypto-js");
+const Encrypt= require("..\\javascript\\encryption.js");
 
-const EncryptTest = require("./tests/encryptTests.js");
-//each test should be given time ms to complete, half of which is timeout time between it and the next test
-const time=100;
+class EncryptTest {
+	static execute(string){
+		Encrypt.initialize();
+		
+		//verify crypto.AES encrypt/decrypt without fs use
+		var i1= crypto.AES.encrypt(string,Encrypt.dataToken).toString();
+		var o1= crypto.AES.decrypt(i1,Encrypt.dataToken);
+		var f1= o1.toString(crypto.enc.Utf8);
+		if(string!=f1){
+			console.log("output from basic encrypt with no fs use failed:\t"+string+"\t"+o1);
+			return -1;
+		}
+		
+		//verify crypto.AES encrypt.decrypt with fs use
+		const filename="testEnc.test";
+		var enc2=crypto.AES.encrypt(string, Encrypt.dataToken).toString();
+		fs.writeFileSync(filename,enc2);
+		var red =fs.readFileSync(filename).toString();
+		if(red!=enc2){
+			console.log("failed to read/write Synced");
+			return -1;
+		}
+		var out2=crypto.AES.decrypt(red,Encrypt.dataToken);
+		var output= out2.toString(crypto.enc.Utf8);
+		if(string !=output){
+			console.log("failed to write encrypted "+string+" to "+filename+" . Output came out as: "+output+"__EOF");
+			return -1;
+		}
+		
+		//verify Encrypt.append which uses fs and enrypt/decrypt internally
+		const app="     asdf CGP Grep | ls";
+		Encrypt.append(filename,app);
+		setTimeout(() => {Encrypt.read(filename,function(data,fn){
+			if(data!=(string+app)){
+				console.log("Failed append");
+				console.log("Append output:\t"+data);
+				console.log("Expected output:\t"+string+app);
+				return -1;
+			}
+		});}, 20);
+
+		
+		return 0;
+	}
+}
 
 
-function test(n){err+=Math.abs(n);}
-
-
-var err=0;//count of errors
-var n=0;//count of tests
-
-
-/**Encryption Tests*/
-setTimeout(() => { 
-	test(EncryptTest.execute("the cold rain and snow"));
-}, time*n++);
-setTimeout(() => { 
-	test(EncryptTest.execute("there were two blackbirds UP A TREE; hauling up the Aris liiine."+
-	" {}{}{{}{}{}{}{}{}{})()()()({{}}(}{)(}{)(}{)({}()}{)(}{)(}{)(}{)({})(}{)(}{)(}{)(}{)(}{)(}{)({})(?><NIOUASPOI~~~```+"+
-	"+poijasdfAT THE LINE  \n Says the other one what do I see at the line, at the liiiine? They slept in mud and wind and rain."+
-	" Cursed the cold and cried in pain; Were cut down like these Roman treees..\\. Poor Michael's up to calvary. \n\n\t\t. My memory it does reveal"));
-}, time*n++);
-setTimeout(() => { 
-	test(EncryptTest.execute(" "));
-}, time*n++);
-setTimeout(() => { 
-	test(EncryptTest.execute(""));
-}, time*n++);
-setTimeout(() => { 
-	test(EncryptTest.execute(null));
-}, time*n++);
-
-
-//log results once enough time has passed for all tests to complete
-setTimeout(() => { 
-	if(err==0)
-		console.log("All tests passed");
-}, time*n++);
+module.exports = EncryptTest;
