9307e324911dd1693add99fb8024c2337d869232
Dragonite <mpeng@optimum.net>
2020-06-30 23:21:23 -0400

    experience math

---
+++ javascript/skinnerbox.js
@@ -0,0 +1,16 @@
+"use strict";
+const Skarm = require("./skarm.js");
+
+const EXP_OFFSET = 100;
+const EXP_SCALE = 50;
+const EXP_BASE = 1.6;
+
+module.exports = {
+    getLevel: function(exp) {
+        return Math.log(Math.max((exp - EXP_OFFSET), 1) / EXP_SCALE) / Math.log(EXP_BASE);
+    },
+    
+    getMinEXP: function(level) {
+        return EXP_SCALE * Math.pow(level, EXP_BASE) + EXP_OFFSET;
+    },
+};
