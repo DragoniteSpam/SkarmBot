992dcc94c5734a150fd73041f006d978730cc709
Dragonite <mpeng@optimum.net>
2020-06-30 23:22:14 -0400

    exp constants are part of the class

--- javascript/skinnerbox.js
+++ javascript/skinnerbox.js
@@ -1,11 +1,11 @@
 "use strict";
 const Skarm = require("./skarm.js");
 
-const EXP_OFFSET = 100;
-const EXP_SCALE = 50;
-const EXP_BASE = 1.6;
-
 module.exports = {
+    const EXP_OFFSET: 100,
+    const EXP_SCALE: 50,
+    const EXP_BASE: 1.6,
+    
     getLevel: function(exp) {
         return Math.log(Math.max((exp - EXP_OFFSET), 1) / EXP_SCALE) / Math.log(EXP_BASE);
     },
