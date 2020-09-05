d72cd91d2ad3a63cbfc6a9ff17979c1978d854b6
Dragonite <mpeng@optimum.net>
2020-09-05 01:57:58 -0400

    pretty error messages for xkcd

--- javascript/xkcd.js
+++ javascript/xkcd.js
@@ -6,81 +6,100 @@ const Encrypt = require("./encryption.js");
 const xkcddb = "..\\skarmData\\xkcd.penguin";
 
 class XKCD {
-    constructor(bot,instance) {
-		this.bot=bot;
+    constructor(bot, instance) {
+		this.bot = bot;
 		this.initialize();
-		this.interval=null;
+		this.interval = null;
         this.schedule();
-		this.lock=0;
-		this.instance=instance;
-		this.references=JSON.parse(fs.readFileSync(".\\fun\\xkcd-log.json").toString().toLowerCase());
-		console.log(JSON.stringify(this.references).substring(0,200));
+		this.lock = 0;
+		this.instance = instance;
+		this.references = JSON.parse(fs.readFileSync(".\\fun\\xkcd-log.json").toString().toLowerCase());
     }
     
-	save(){
+	save() {
 		Encrypt.write(xkcddb, JSON.stringify(this.bot.channelsWhoLikeXKCD));
 		console.log("Saved XKCD Data");
 	}
 	
-	initialize(){
+	initialize() {
 		var tis = this;
 		Encrypt.read(xkcddb, function(data, filename) {
             tis.bot.channelsWhoLikeXKCD = JSON.parse(data);
-			console.log("Initialized "+Object.keys(tis.bot.channelsWhoLikeXKCD).length + " XKCD channels on Instance "+tis.instance);
+			console.log("Initialized " + Object.keys(tis.bot.channelsWhoLikeXKCD).length + " XKCD channels on Instance " + tis.instance);
         });
 	}
 	
-	poisonPill(){
+	poisonPill() {
 		clearInterval(this.interval);
 	}
 	
     schedule() {
-		if(!this.interval){
+		if (!this.interval) {
 			clearInterval(this.interval);
 		}
-        this.interval = setInterval(this.sweep, 1000 * 60*60);
+        this.interval = setInterval(this.sweep, 1000 * 60 * 60);
     }
     
     post(channel, id) {
-        id = id || "";
+        id = (id || "").toLowerCase();
         if (id.match(/\d+/)) {
             Skarm.sendMessageDelay(channel, "https://xkcd.com/" + id + "/");
 			return;
         }
-		if (id == "") {
+		if (id === "") {
             Skarm.sendMessageDelay(channel, "https://xkcd.com/");
 			return;
         }
-		let results = [];
-		for(let i in this.references.alphabetized){
-			//console.log(this.references.alphabetized[i][0]);
-			if(this.references.alphabetized[i][0].includes(id)){
-				results.push("https://xkcd.com/" + this.references.alphabetized[i][1] + "/");
+        // the name IDs have already been loaded in lower case
+		let results = [ ];
+		for (let i in this.references.alphabetized) {
+            let data = this.references.alphabetized[i];
+            // is an exact match?
+            if (data[0] === id) {
+                results = [[data[0], "https://xkcd.com/" + data[1] + "/"]];
+                break;
+            }
+            // is a missal of silos?
+			if (data[0].includes(id)) {
+				results.push([data[0], "https://xkcd.com/" + data[1] + "/"]);
 			}
 		}
-		if(results.length>1){
-			for(let i in results){
-				results[i]="<"+results[i]+">";
+        
+		if (results.length === 0) {
+			Skarm.sendMessageDelay(channel, "I couldn't find any xkcds containing **" + id + "** in the title\nヽ( ｡ ヮﾟ)ノ");
+			return;
+		}
+        
+		if (results.length === 1 && results[0][0] === id) {
+			Skarm.sendMessageDelay(channel,results[0][1]);
+			return;
+		}
+        
+		if (results.length > 1) {
+            const MAX_RESULTS = 10;
+            let overflow = results.length - MAX_RESULTS;
+            let printresults = [ ];
+			for (let i = 0; i < Math.min(results.length, MAX_RESULTS); i++) {
+				printresults[i] = "**" + results[i][0] + "** <" + results[i][1] + ">";
 			}
-			Skarm.sendMessageDelay(channel,"Could not find an exact match, try one of these: <"+results.join("\n"));
+            let message = "Could not find an exact match for **" + id + "**, try one of these: \n" + printresults.join("\n");
+            if (overflow > 0) {
+                message += "\n(And " + overflow + " more)";
+            }
+			Skarm.sendMessageDelay(channel, message);
 			return;
 		}
-		if(results.length==1){
-			Skarm.sendMessageDelay(channel,results[0]);
-			return;
-		}
-
-		Skarm.sendMessageDelay(channel, "I'm not sure what you mean.");
+        
 		return;
     }
 	
 	sweep(n) {
 		var d = new Date(); // for now
 		var datetext = d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
-		console.log("Running xkcd.sweep function on Instance "+this.instance +"\tCurrent time: "+datetext);
+		console.log("Running xkcd.sweep function on Instance "+this.instance + "\tCurrent time: " + datetext);
 		let now = new Date();
-		if (this.lock <1 &&(n || now.getHours() == 20 && (now.getDay()&1))) {
-			this.lock = 3 + ((n)?10:0);
+		if (this.lock < 1 && (n || now.getHours() === 20 && (now.getDay() & 1))) {
+			this.lock = 3 + (n ? 10 : 0);
 			for(var channel in this.bot.channelsWhoLikeXKCD){
 				this.post(this.bot.client.Channels.get(channel));
 			}
