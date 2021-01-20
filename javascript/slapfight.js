8eb34f39b1694e90255e54799ab6e484f839b3e9
Dragonite <mpeng@optimum.net>
2021-01-20 00:22:36 -0500

    added slapfight

---
+++ javascript/slapfight.js
@@ -0,0 +1,12 @@
+"use strict";
+const fs = require("fs");
+
+const Skarm = require("./skarm.js");
+const Guild = require("./guild.js");
+const User = require("./users.js");
+
+class SlapFight {
+    
+}
+
+module.exports = SlapFight;
