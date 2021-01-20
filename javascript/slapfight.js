f6297a240bb48e975d0d7f2117e38a2d31ad63ed
Dragonite <mpeng@optimum.net>
2021-01-20 00:39:33 -0500

    slapfight constructor

--- javascript/slapfight.js
+++ javascript/slapfight.js
@@ -3,10 +3,18 @@ const fs = require("fs");
 
 const Skarm = require("./skarm.js");
 const Guild = require("./guild.js");
-const User = require("./users.js");
+const User = require("./user.js");
 
 class SlapFight {
-    
+    constructor(challenger, opponent, channel) {
+        this.challenger = challenger;
+        this.opponent = opponent;
+        this.health = { };
+        this.health[challenger] = 10;
+        this.health[opponent] = 10;
+        Skarm.sendMessageDelay(channel, "Battle started: **" + challenger.name +
+            "** vs. **" + opponent.name + "!**");
+    };
 }
 
 module.exports = SlapFight;
