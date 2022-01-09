dafbb2b065065ad468b44ab08601b947a7d0e0dc
MSNelyubov <36687789+ArgoTheNaut@users.noreply.github.com>
2022-01-09 14:41:34 -0500

    cloned npm initialization segment from Sonine
    Source: https://github.com/ArgoTheNaut/sonine/blob/main/initialize-NodeModules.ps1

---
+++ initialize-Dependencies.ps1
@@ -0,0 +1,52 @@
+﻿Param(
+    [switch]$Force = $false
+)
+
+Function reportGood($value){
+    Write-Host -Object $value -ForegroundColor Green
+}
+
+Function reportWarn($value){
+    Write-Host -Object $value -ForegroundColor Yellow -BackgroundColor Red
+}
+
+$npmRoot = "$PSScriptRoot\node_modules"
+
+Push-Location
+
+cd $PSScriptRoot
+
+if($Force){
+    Write-Host "Purging $npmRoot"
+    Remove-Item -Recurse -Force $npmRoot
+}
+
+$PackageList = @("child_process", "discordie", "request", "node-wolfram", "crypto-js")
+
+$PackageList | foreach {
+    $item = $_
+    
+    #NPM package paths ignore @ suffix
+    if($_.contains("@")){
+        $item = $item.Substring(0,$item.indexOf("@"))
+    }
+    
+    
+    if(Test-Path $npmRoot\$item){
+        reportGood("Initialized $item package")
+        return;
+    }else{
+        reportWarn("installing package: $_")
+        npm install $_ >>$null 2>$Null
+    }
+
+
+    if(Test-Path $npmRoot\$item){
+        reportGood("Initialized $item package")
+    }else{
+        reportWarn("Failed to find package: $npmRoot\$item")
+        reportWarn("Please run 'npm install $_'")
+    }
+}
+
+Pop-Location
