8a3cf5ead650b46a48342aa7cd43b83f540fb4a1
MSNelyubov <36687789+ArgoTheNaut@users.noreply.github.com>
2022-01-09 14:46:26 -0500

    launcher tests for node modules before beginning execution

--- launcher.ps1
+++ launcher.ps1
@@ -1,4 +1,9 @@
-﻿cd $PSScriptRoot
+﻿Push-Location
+cd $PSScriptRoot
+if(-not (Test-Path "$PSScriptRoot\node_modules")){
+    Start-Process -Wait powershell.exe -ArgumentList @("$PSScriptRoot\initialize-Dependencies.ps1")
+}
 do{
     node $PSScriptRoot\bot.js beta
 }while($LASTEXITCODE -ne 69)
+Pop-Location
