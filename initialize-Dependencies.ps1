8d950785e3768af9471118f1c95764c1a5f96cfa
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-01-12 20:50:26 -0500

    added installation of node.js to initialization script

--- initialize-Dependencies.ps1
+++ initialize-Dependencies.ps1
@@ -21,6 +21,61 @@ if($Force){
     Remove-Item -Recurse -Force $npmRoot
 }
 
+#initialize webclient and temp directory (not checked into git)
+$webClient = New-Object System.Net.WebClient
+$webPageFilePath = "$PSScriptRoot/temp/site.html"
+if(-not (Test-Path "$PSScriptRoot/temp")){
+    $tempDir = mkdir temp
+}
+
+#Fetch latest version number of nodejs
+$VersionSite = "https://nodejs.org/en/"
+Remove-Item -Force $webPageFilePath -ErrorAction SilentlyContinue
+$webClient.DownloadFile($VersionSite, $webPageFilePath)
+
+#typecasting
+$fileData = (Get-Content -Path $webPageFilePath) | where {$_.Contains("nodejs.org/dist/v")}
+if($fileData.GetType().BaseType.Name -eq "Array"){
+    $fileData = $fileData[0]
+}
+
+#isolate version number from file data
+$fileData = $fileData.Substring($fileData.IndexOf("dist/v")+"dist/v".Length)
+$fileData = $fileData.Substring(0,$fileData.IndexOf("/"))
+$nodejsVersion = $fileData
+
+#construct node download URLs
+$nodejsDownloadBase = 'https://nodejs.org/dist/v'+$nodejsVersion+'/'
+$nodejsFile = 'node-v'+$nodejsVersion+'-x64.msi'
+$nodejsDownloadLink = $nodejsDownloadBase+$nodejsFile
+$nodejsFileFull = "$PSScriptRoot/temp/$nodejsFile"
+
+#check currently installed version in registry
+$nodeProducts = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | where {$_.DisplayName -and $_.DisplayName.Contains("Node")}
+
+
+if(!$nodeProducts -or ($nodeProducts.DisplayVersion -ne $nodejsVersion)){
+    Write-Host "Installing node.js version $nodejsVersion..."
+    #SRC: https://gist.github.com/manuelbieh/4178908#file-win32-node-installer
+    Remove-Item -Force $nodejsFileFull
+    $webClient.DownloadFile($nodejsDownloadLink, $nodejsFileFull)
+    if([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'){                     #https://megamorf.gitlab.io/2020/05/26/check-if-powershell-is-running-as-administrator/
+        msiexec /passive /log "$nodejsVersion.log" /package $nodejsFileFull
+    }else{
+        reportWarn "Session is not running as admin.  Unable to install the latest version of node js"
+        
+        if($nodeProducts){
+            reportWarn ("Rolling with existing version: " + $nodeProducts.DisplayVersion)
+        }else{
+            exit        #don't bother checking NPM packages if node is not installed
+        }
+    }
+}else{
+    reportGood "the latest version of node is already installed $nodejsVersion"
+}
+
+
+
 $PackageList = @("child_process", "discordie", "request", "node-wolfram", "crypto-js")
 
 $PackageList | foreach {
