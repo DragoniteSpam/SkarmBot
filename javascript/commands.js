266905a7ce47ec888c43775483d6d0fd08f6e40a
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-02-02 22:17:51 -0500

    srank documentation, - to clear entry

--- javascript/commands.js
+++ javascript/commands.js
@@ -1475,7 +1475,11 @@ Random quotes are from Douglas Adams, Terry Pratchett, Arthur C. Clark, Rick Coo
 		aliases: ["srank","slevel"],
         params: ["<@targetID>, exp"],
         usageChar: "@",
-        helpText: "sets how much exp you have in the guild",
+        helpText: "Sets how much exp you have in the guild\r\n" +
+            "Example 0: `e@srank 256` sets your own experience points to 256.  Enough to achieve level 2!\r\n" +
+            "Example 1: `e@srank @magikarp#1234 0` sets the experience points of the user @magikarp#1234 to 0\r\n" +
+            "Example 2: `e@srank @Dragonite#7992 100` sets the experience points of the user `@Dragonite#7992` to 100.  Enough to achieve level 1!\r\n" +
+            "Example 3: `e@srank @Dragonite#7992 -` removes dragonite's record from the exp table.  Future message by dragonite will re-add him to the table.",
         ignoreHidden: true,
 		category: "leveling",
         perms: Permissions.MOD,
@@ -1519,15 +1523,20 @@ Random quotes are from Douglas Adams, Terry Pratchett, Arthur C. Clark, Rick Coo
 			target = target || e.message.author.id;
 			let user = guildData.expTable[target];
 			if(user) {
-			    if(!isNaN(newExp - 0))
-                    user.exp = newExp;
-                user.level = Skinner.getLevel(user.exp);
-                user.nextLevelEXP = Skinner.getMinEXP(user.level);
-                Skarm.sendMessageDelay(e.message.channel, "New rank data for <@"+ (target)+">\n>>> New total EXP: " + user.exp + "\nEXP required to go for next level: " + (user.nextLevelEXP - user.exp) + "\nCurrent level: " + user.level);
-                let guildMembers = e.message.guild.members;
-                for(let member of guildMembers){
-                    if(member.id === target){
-                        guildData.roleCheck(member, user);
+			    if(newExp === "-"){
+                    delete guildData.expTable[target];
+                    Skarm.sendMessageDelay(e.message.channel, "User data purged.");
+                }else {
+                    if (!isNaN(newExp - 0))
+                        user.exp = newExp;
+                    user.level = Skinner.getLevel(user.exp);
+                    user.nextLevelEXP = Skinner.getMinEXP(user.level);
+                    Skarm.sendMessageDelay(e.message.channel, "New rank data for <@"+ (target)+">\n>>> New total EXP: " + user.exp + "\nEXP required to go for next level: " + (user.nextLevelEXP - user.exp) + "\nCurrent level: " + user.level);
+                    let guildMembers = e.message.guild.members;
+                    for(let member of guildMembers){
+                        if(member.id === target){
+                            guildData.roleCheck(member, user);
+                        }
                     }
                 }
             }else{
