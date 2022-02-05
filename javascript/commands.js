0c13745e8c374a6ee5bda34f9410a631df719f23
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-02-05 16:18:05 -0500

    added examples for e!summons

--- javascript/commands.js
+++ javascript/commands.js
@@ -110,7 +110,8 @@ module.exports = {
         examples: [
             {command: "e!pinned", effect: "Will cause Skarm to report the amount of pinned messages in the channel that it is run in."},
             {command: "e!fetchpinned #general", effect: "Will cause Skarm to report the amount of pinned messages in the channel #general."}
-        ],		ignoreHidden: true,
+        ],
+        ignoreHidden: true,
         category: "general",
 
         execute(bot, e, userData, guildData) {
@@ -150,6 +151,24 @@ module.exports = {
         usageChar: "!",
         helpText: "Skarm can be asked to send you notifications for messages with certain keywords (often your username, or other topics you like to know about - for example, \"Wooloo\" or \"programming\"). You can add, remove, or list your summons." +
             "\nMessages that skarm sends containing your summons will be deleted after 15 seconds (30 seconds for e!summons list) or immediately by clicking \u274c.",
+        examples: [
+            {
+                command: "e!summons add jeff",
+                effect: "Will cause Skarm to notify you whenever `jeff` appears in a message."
+            },
+            {
+                command: "e!summons add skarmory skarmbot skram skarm",
+                effect: "Will cause Skarm to notify you if any of the four terms listed appear in a messasge."
+            },
+            {
+                command: "e!summons remove jeff",
+                effect: "Will stop Skarm from notifying you whenever `jeff` appears in a message."
+            },
+            {
+                command: "e!summon list",
+                effect: "Will cause Skarm to report the list of terms that you will be notified about."
+            }
+        ],
         ignoreHidden: true,
         category: "general",
 
@@ -157,42 +176,42 @@ module.exports = {
             let params = commandParamTokens(e.message.content.toLowerCase());
             let action = params[0];
             let term;
-            if(params.length){
-				term = params[1];
-			}else{
-				term = "";
-			}
-			let retina = "";
+            if (params.length) {
+                term = params[1];
+            } else {
+                term = "";
+            }
+            let returnString = "";
             if (action === "add") {
-				for(let i=1;i<params.length;i++){
-					if (userData.addSummon(params[i].replace(",",""))) {
-						retina+= "**" + params[i] + "** is now a summon for " + e.message.author.username + "!\n";
-					} else {
-						retina+= "Could not add the term " + params[i] + " as a summon. (Has it already been added?)\n";
-					}
-				}
-				Skarm.sendMessageDelete(e.message.channel,retina,false,null,15000,e.message.author.id,bot);
+                for (let i = 1; i < params.length; i++) {
+                    if (userData.addSummon(params[i].replace(",", ""))) {
+                        returnString += "**" + params[i] + "** is now a summon for " + e.message.author.username + "!\n";
+                    } else {
+                        returnString += "Could not add the term " + params[i] + " as a summon. (Has it already been added?)\n";
+                    }
+                }
+                Skarm.sendMessageDelete(e.message.channel, returnString, false, null, 15000, e.message.author.id, bot);
                 return;
             }
             if (action === "remove") {
-				for(let i=1;i<params.length;i++){
-					if (userData.removeSummon(params[i].replace(",",""))) {
-						retina+= "**" + params[i] + "** is no longer a summon for " + e.message.author.username + "!\n";
-					} else {
-						retina+= "Could not remove the term " + params[i] + " as a summon. (Does it exist in the summon list?)\n";
-					}
-				}
-				Skarm.sendMessageDelete(e.message.channel,retina,false,null,15000,e.message.author.id,bot);
+                for (let i = 1; i < params.length; i++) {
+                    if (userData.removeSummon(params[i].replace(",", ""))) {
+                        returnString += "**" + params[i] + "** is no longer a summon for " + e.message.author.username + "!\n";
+                    } else {
+                        returnString += "Could not remove the term " + params[i] + " as a summon. (Does it exist in the summon list?)\n";
+                    }
+                }
+                Skarm.sendMessageDelete(e.message.channel, returnString, false, null, 15000, e.message.author.id, bot);
                 return;
             }
             if (action === "list") {
                 let summonString = userData.listSummons(term);
                 if (summonString.length === 0) {
-					retina+="**" + e.message.author.username + "**, you currently have no summons!";
+                    returnString += "**" + e.message.author.username + "**, you currently have no summons!";
                 } else {
-                    retina+= "**" + e.message.author.username + "**, your current summons are:\n```" + summonString + "```";
+                    returnString += "**" + e.message.author.username + "**, your current summons are:\n```" + summonString + "```";
                 }
-				Skarm.sendMessageDelete(e.message.channel,retina,false,null,30000,e.message.author.id,bot);
+                Skarm.sendMessageDelete(e.message.channel, returnString, false, null, 30000, e.message.author.id, bot);
                 return;
             }
             Skarm.erroneousCommandHelpPlease(e.message.channel, this);
