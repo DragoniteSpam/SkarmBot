bdd019758107e2909d63ad4d4ac19d6f858e8a17
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-02-05 19:40:28 -0500

    e!help google

--- javascript/commands.js
+++ javascript/commands.js
@@ -912,6 +912,9 @@ Random quotes are from Douglas Adams, Sean Dagher, The Longest Johns, George Car
         params: ["query..."],
         usageChar: "!",
         helpText: "Returns the results of a web search of the specified query. The `cosia` alias is an acceptable usage of punning.",
+        examples: [
+            {command: "e!google sonder definition", effect: "Provides a link to a search engine query for `sonder definition`"}
+        ],
         ignoreHidden: true,
         category: "web",
 
