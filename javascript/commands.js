5856054c1d7abfa798a8bd60c2b895f1bca4219c
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-02-05 19:46:13 -0500

    e!help xkcd

--- javascript/commands.js
+++ javascript/commands.js
@@ -962,12 +962,17 @@ Random quotes are from Douglas Adams, Sean Dagher, The Longest Johns, George Car
     //         Skarm.help(this, e);
     //     },
     // },
-
     XKCD: {
         aliases: ["xkcd"],
         params: ["[id]"],
         usageChar: "!",
         helpText: "Returns the XKCD with the specified ID; if no ID is specified, it will return the latest strip instead. ID may be an index or a strip name.",
+        examples: [
+            {command: "e!xkcd ", effect: "Provides a link to the most recent XKCD comic."},
+            {command: "e!xkcd 753", effect: "Provides a link to XKCD 753."},
+            {command: "e!xkcd compiling", effect: "Provides a link to the xkcd titled `compiling`."},
+            {command: "e!xkcd web", effect: "Provides a link for every xkcd containing `web` in its title."}
+        ],
         ignoreHidden: true,
 		category: "web",
 
