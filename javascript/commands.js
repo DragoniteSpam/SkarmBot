7382b977b38ee7e6102d07acb78e367831a38e9d
Argo <36687789+ArgoTheNaut@users.noreply.github.com>
2022-06-21 21:55:08 -0400

    tabs

--- javascript/commands.js
+++ javascript/commands.js
@@ -617,29 +617,29 @@ module.exports = {
         execute(bot, e, userData, guildData) {
             let version = Math.floor(Math.random() * 0xffffffff);
             Skarm.sendMessageDelay(e.message.channel,
-`**Skarm Bot 2**\n
-Lead spaghetti chef: Dragonite#7992
-Seondary spaghetti chef: ArgoTheNaut#9716
-Version: ${version}
-
-Library: Discordie (JavaScript):
-<https://qeled.github.io/discordie/#/?_k=m9kij6>
-
-Dragonite:
-<https://www.youtube.com/c/dragonitespam>
-<https://github.com/DragoniteSpam/SkarmBot>
-
-Argo:
-<https://github.com/ArgoTheNaut>
-
-Extra ideas came from SuperDragonite2172, willofd2011, and probably other people.
-
-Thanks to basically everyone on the Kingdom of Zeal server for testing this bot, as well as all of the people who Argo somehow tricked into worshipping him as their god-king.
-
-Wolfram-Alpha is awesome:
-<https://www.npmjs.com/package/node-wolfram>
-
-Random quotes are from Douglas Adams, Sean Dagher, The Longest Johns, George Carlin, Terry Pratchett, Arthur C. Clark, Rick Cook, and The Elder Scrolls V: Skyrim.`
+            `**Skarm Bot 2**\n
+            Lead spaghetti chef: Dragonite#7992
+            Seondary spaghetti chef: ArgoTheNaut#9716
+            Version: ${version}
+            
+            Library: Discordie (JavaScript):
+            <https://qeled.github.io/discordie/#/?_k=m9kij6>
+            
+            Dragonite:
+            <https://www.youtube.com/c/dragonitespam>
+            <https://github.com/DragoniteSpam/SkarmBot>
+            
+            Argo:
+            <https://github.com/ArgoTheNaut>
+            
+            Extra ideas came from SuperDragonite2172, willofd2011, and probably other people.
+            
+            Thanks to basically everyone on the Kingdom of Zeal server for testing this bot, as well as all of the people who Argo somehow tricked into worshipping him as their god-king.
+            
+            Wolfram-Alpha is awesome:
+            <https://www.npmjs.com/package/node-wolfram>
+            
+            Random quotes are from Douglas Adams, Sean Dagher, The Longest Johns, George Carlin, Terry Pratchett, Arthur C. Clark, Rick Cook, and The Elder Scrolls V: Skyrim.`
             );
         },
         
