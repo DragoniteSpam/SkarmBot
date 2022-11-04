be7d7aefbddf16173d64ea214ff9f86459d7adbd
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2022-11-03 23:03:10 -0400

    implemented repo weight editing

--- javascript/commands.js
+++ javascript/commands.js
@@ -2057,12 +2057,14 @@ module.exports = {
         usageChar: "@",
         helpText: "Configures the keywords that skarm will respond to, how he will respond, and how likely he is to respond.",
         examples: [
-            {command: "e@parrot",                 effect: "Will list the available keyword tables that can be configured."},
-            {command: "e@parrot skyrim",          effect: "Will list the weights for keywords in the `skyrim` table.  Messages sent with keyword weights that add up to >1 will guarantee that skarm responds."},
-            {command: "e@parrot skyrim jarl 0.3", effect: "Sets the probability of skarm responding upon hearing `jarl` to 0.3"},
-            {command: "e@parrot e scaling 0",     effect: "Sets the degree to which the size of the quote repo affects the probability that it will be drawn from when `everything` is called.\r\n0 -> everything weights are independent of size. 1 -> the more lines there are in a quote repo, the higher the probability that it will be drawn from (linearly growing share)\r\nCalling this command without the number will return the current value."},
-            {command: "e@parrot e w",             effect: "Will list the weights for how the everything distribution is factored into the other repositories."},
-            {command: "e@parrot e w skyrim 0.3",  effect: "Will set the weight for the skyrim distribution to receive 0.3 shares of the distribution for every sum total of weights."},
+            {command: "e@parrot",                        effect: "Will list the available keyword tables that can be configured."},
+            {command: "e@parrot skyrim",                 effect: "Will list the weights for keywords in the `skyrim` table.  Messages sent with keyword weights that add up to >1 will guarantee that skarm responds."},
+            {command: "e@parrot skyrim jarl 0.3",        effect: "Sets the probability of skarm responding upon hearing `jarl` to 0.3"},
+            {command: "e@parrot e scaling 0",            effect: "Sets the degree to which the size of the quote repo affects the probability that it will be drawn from when `everything` is called.\r\n0 -> everything weights are independent of size. 1 -> the more lines there are in a quote repo, the higher the probability that it will be drawn from (linearly growing share)\r\nCalling this command without the number will return the current value."},
+            {command: "e@parrot e w",                    effect: "Will list the weights for how the everything distribution is factored into the other repositories."},
+            {command: "e@parrot e w skyrim 0.3",         effect: "Will set the weight for the skyrim distribution to receive 0.3 shares of the distribution for every sum total of weights."},
+            {command: "e@parrot factory-reset",          effect: "Will reset the weights of all repositories to the defaults that skarm started off with."},
+            {command: "e@parrot factory-reset skyrim",   effect: "Will reset the weights of all of the skyrim weights to the defaults that skarm started off with."},
         ],
         ignoreHidden: false,
         perms: Permissions.MOD,
@@ -2091,19 +2093,32 @@ module.exports = {
             // operations on a specific repo
             if (action in quoteRepos) {
                 let quoteRepoTerm = tokens.shift();    // take next token to determine what to act on
+                let repoKeywordWeightMapping = quoteRepos[action];
 
                 // if no term is specified, list current config
                 if (quoteRepoTerm === undefined) {
-                    let repoData = quoteRepos[action];
                     outputString = `Weights attributed to each keyword in \`${action}\`.  Weights that add up to at least 1 will guarantee skarm responds.`;
-                    for (let keyword in repoData) {
-                        fields.push({name: `${keyword}`, value: `${repoData[keyword]}`, inline: true});
+                    for (let keyword in repoKeywordWeightMapping) {
+                        fields.push({name: `${keyword}`, value: `${repoKeywordWeightMapping[keyword]}`, inline: true});
                     }
-                }
+                } else {
+                    // if a term is specified, make sure it's valid then make the adjustment
+                    // i.e. `e@parrot skyrim whiterun 1` sets the probability of whiterun being triggered to 1
+                    // i.e. `e@parrot skyrim whiterun 0` removes whiterun from the word list for skyrim
+                    console.log("repo data:", repoKeywordWeightMapping);
+                    if(tokens.length === 1){
+                        let newVal = tokens.shift() - 0;
+                        if(!isNaN(newVal)) {
+                            repoKeywordWeightMapping[quoteRepoTerm] = newVal; // use the user input only if it is valid
+                        }
+                        outputString = `Weight of term \`${quoteRepoTerm}\` set to \`${repoKeywordWeightMapping[quoteRepoTerm]}\``;
+                    } else {
+                        outputString = `The weight of term \`${quoteRepoTerm}\` is currently set to \`${repoKeywordWeightMapping[quoteRepoTerm]}\``;
+                    }
 
-                // todo: if a term is specified, make sure it's valid then make the adjustment
-                // i.e. `e@parrot skyrim whiterun 1` sets the probability of whiterun being triggered to 1
-                // i.e. `e@parrot skyrim whiterun 0` removes whiterun from the word list for skyrim
+                }
+                
+                // clean up namespace of 0's?
             }
 
             //             {command: "e@parrot e w",             effect: "Will list the weights for how the everything distribution is factored into the other repositories."},
@@ -2116,6 +2131,10 @@ module.exports = {
             if (action === "scaling") {
                 // todo: read and adjust scaling
             }
+
+
+            //todo             {command: "e@parrot factory-reset",   effect: "Will reset the weights of all repositories to the defaults that skarm started off with."},
+            //todo             {command: "e@parrot factory-reset skyrim",   effect: "Will reset the weights of all of the skyrim weights to the defaults that skarm started off with."},
 
 
             Skarm.sendMessageDelay(e.message.channel, " ", false, {
