399f34d2ae120941dd4b7749b3821b565f96f375
Argo <36687789+ArgoTheNaut@users.noreply.github.com>
2023-07-17 18:34:52 -0400

    fixed dependencies

--- javascript/commands/_imports.js
+++ javascript/commands/_imports.js
@@ -1,16 +1,16 @@
 const os = require("os");
 const request = require("request");
 
-const Skarm = require("./skarm.js");
-const Constants = require("./constants.js");
-const Web = require("./web.js");
-const Users = require("./user.js");
-const Guilds = require("./guild.js");
-const Permissions = require("./permissions.js");
-const Skinner = require("./skinnerbox.js");
+const Skarm = require("../skarm.js");
+const Constants = require("../constants.js");
+const Web = require("../web.js");
+const Users = require("../user.js");
+const Guilds = require("../guild.js");
+const Permissions = require("../permissions.js");
+const Skinner = require("../skinnerbox.js");
 
-const SarGroups = require("./guildClasses/sar.js");
-const {ShantyCollection} = require("./shanties");
+const SarGroups = require("../guildClasses/sar.js");
+const {ShantyCollection} = require("../shanties");
 
 Constants.initialize();     // if this line isn't here, local initialization of constants in "effect" fields break
 
