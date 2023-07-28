b8e29f0ef42eb0740a84f1772fcc5715763a72fb
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2023-07-27 21:19:42 -0400

    Autopin (bugfix) (#62)
    * WIP: created class, need command interface to enable
    
    * moved comments
    
    * Create Autopin.js
    
    * adjusted class methods
    
    * framework for first-layer responses
    
    * Implemented remaining command structure
    
    * spacing
    
    * only text channels
    
    * clear/disable flip flop, casing on disabled flag
    
    * refining pinned message format
    
    * resolving cleanup details
    
    * final tweaks

--- javascript/guildClasses/autopin.js
+++ javascript/guildClasses/autopin.js
@@ -46,13 +46,11 @@ let linkFunctions = function (autoPin){
 
     autoPin.disableDirectForward = function (channelId) {
         let fwd = autoPin.getDirectForward(channelId);
-        if (!fwd) {
+        if (fwd) {
+            delete autoPin.directForwarding[channelId];
+        } else {
             autoPin.directForwarding[channelId] = DISABLED_FLAG;
         }
-        if (fwd && fwd !== DISABLED_FLAG) {
-            delete autoPin.directForwarding[channelId];
-        }
-
     }
 
     autoPin.enable = function () {autoPin.enabled = true;};
@@ -87,9 +85,10 @@ let linkFunctions = function (autoPin){
     };
 
     autoPin.cyclePins = function (channelObj) {
+        if (!Skarm.hasMessageAccess(channelObj)) return;
         if (!autoPin.enabled) return;
         let destination = autoPin.getForward(channelObj.id);
-        if(!destination) return;
+        if (!destination) return;
 
         channelObj.fetchPinned().then(response => {
             let pins = response.messages;
@@ -102,7 +101,7 @@ let linkFunctions = function (autoPin){
             let description = `Link: ${messageLink}\n\n${oldestPin.content}\n`;
 
             let imageUrl = undefined;
-            if(oldestPin.attachments){
+            if(oldestPin.attachments && oldestPin.attachments.length){
                 let attachmentStr = "\n\nAttachments:\n";
                 for(let attachment of oldestPin.attachments) {
                     // console.log(attachment);
