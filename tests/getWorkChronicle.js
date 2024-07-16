706e17e825b42fcaa1d8774f1b032e89465429c5
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2024-07-16 00:02:39 -0400

    paramater variant testing

--- tests/getWorkChronicle.js
+++ tests/getWorkChronicle.js
@@ -1,10 +1,16 @@
 
-fetch("https://workchronicles.substack.com/api/v1/archive?sort=new&search=&offset=191&limit=12", {
+fetch("https://workchronicles.substack.com/api/v1/archive?sort=new&search=&offset=1&limit=50", {
   "body": null,
   "method": "GET"
 }).then((response) => {
     return response.json();
 }).then((comics)=>{
-    let {title, slug, post_date, canonical_url, cover_image} = comics[0];
-    console.log(title, slug, post_date, canonical_url, cover_image);
+    comics
+        .filter(c=>c.title.includes("(comic)"))
+        .map((c) => {
+        console.log(c.post_date, c.title, c.slug);
+    })
+    // let {title, slug, post_date, canonical_url, cover_image} = comics[0];
+    // console.log(title, slug, post_date, canonical_url, cover_image);
+    console.log(comics[0]);
 });
