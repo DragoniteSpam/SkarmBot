a46ca45ccd7bc40f83bd56b5cba3b721c3894988
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2024-07-16 23:52:30 -0400

    polling examples

--- javascript/commands/cmd_Poll.js
+++ javascript/commands/cmd_Poll.js
@@ -1,23 +1,27 @@
 "use strict";
-const {os, request, Skarm, Constants, Web, Users, Guilds, Permissions, Skinner, SarGroups, ShantyCollection} = require("./_imports.js");
+const { os, request, Skarm, Constants, Web, Users, Guilds, Permissions, Skinner, SarGroups, ShantyCollection } = require("./_imports.js");
 
 module.exports = {
-        aliases: ["poll"],
-        params: ["target"],
-        usageChar: "@",
-        helpText: "Polls a comic or all comics.",
-        examples: [{command: "e!ping", effect: "Skarm will send a message, and then edit the message to include the time that it took for the event to be registered."}],
-        ignoreHidden: false,
-        perms: Permissions.MOM,
-        category: "infrastructure",
+    aliases: ["poll"],
+    params: ["target"],
+    usageChar: "@",
+    helpText: "Polls a comic or all comics.",
+    examples: [
+        { command: "e!poll", effect: "Skarm will poll all the comics." },
+        { command: "e!poll XKCD", effect: "Skarm will poll the XKCD channel for new releases." },
+        { command: "e!poll WorkChronicles", effect: "Skarm will poll the WorkChronicles channel for new releases." },
+    ],
+    ignoreHidden: false,
+    perms: Permissions.MOM,
+    category: "infrastructure",
 
-        execute(bot, e, userData, guildData) {
-            Skarm.spam("Polling:", e.message.content);
-            bot.comics.poll(Skarm.commandParamString(e.message.content));
-        },
-        
-        help(bot, e) {
-            Skarm.help(this, e);
-        },
+    execute(bot, e, userData, guildData) {
+        Skarm.spam("Polling:", e.message.content);
+        bot.comics.poll(Skarm.commandParamString(e.message.content));
+    },
+
+    help(bot, e) {
+        Skarm.help(this, e);
+    },
 }
 
