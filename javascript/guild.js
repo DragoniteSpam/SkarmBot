4acea9d794feb0261683ee070ad77c7e114ddc74
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2024-08-15 13:46:18 -0400

    added safeguard around point of failure

--- javascript/guild.js
+++ javascript/guild.js
@@ -807,8 +807,16 @@ const linkFunctions = function(guild) {
         }
         if (notification === Constants.Notifications.NAME_CHANGE) {
             let member = eventObject.member;
-            let oldName= User.get(eventObject.user.id).previousName;
-            User.get(eventObject.user.id).previousName = undefined;   // clear field after use
+            let oldName = "";
+            try {
+                let user = User.get(eventObject.user.id)
+                oldName = user.previousName;
+                user.previousName = undefined;   // clear field after use
+            } catch (error) {
+                Skarm.spam("Caught a name change event that threw an error");
+                console.error(error);
+                return -1;
+            }
             Skarm.logError(`Might be sending out name change notification out to guild: ${JSON.stringify(guild.id)}\n Notification channel list: ${JSON.stringify(guild.notificationChannels)}`);
             console.log(eventObject);
             Skarm.spam("Notification of name change: "+oldName +" -> " + JSON.stringify(eventObject.user));
