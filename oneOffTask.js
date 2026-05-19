2c6f4c6e1385b800ef236b00ca27b6d6984cda3c
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2026-05-19 17:35:38 -0400

    clean up / format oot

--- oneOffTask.js
+++ oneOffTask.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const request = require("request");
 const { spawn } = require("child_process");
 // discord things
-const client = new discordie({autoReconnect:true});
+const client = new discordie({ autoReconnect: true });
 const events = discordie.Events;
 
 // we made these
@@ -18,30 +18,31 @@ const Encrypt = require("./javascript/encryption.js");
 const Constants = require("./javascript/constants.js");
 const Users = require("./javascript/user.js");
 const Guilds = require("./javascript/guild.js");
+const { ShantyCollection } = require('./javascript/shanties.js');
 
 // i'm in?
-var target="../token.txt";
+var target = "../token.txt";
 const token = fs.readFileSync(target).toString();
 
 // javascript
-String.prototype.replaceAll = function(search, replacement) {
-    return this.replace(new RegExp(search, "g"), replacement);
+String.prototype.replaceAll = function (search, replacement) {
+	return this.replace(new RegExp(search, "g"), replacement);
 };
 
 Constants.initialize();
 
-client.connect({token: token});
+client.connect({ token: token });
 
 let uptimeController = {
-	connectTime:     0,
-	disconnectTime:  0
+	connectTime: 0,
+	disconnectTime: 0
 };
 
 let bot;
 
 // try to put all of the actual event code in skarmbot.js to keep this main
 // file clean
-client.Dispatcher.on(events.GATEWAY_READY, e => {
+client.Dispatcher.on(events.GATEWAY_READY, async (e) => {
 	if (bot) {
 		uptimeController.connectTime = Date.now();
 		Skarm.log("Came back online after " + (uptimeController.connectTime - uptimeController.disconnectTime) / 1000 + " seconds down");
@@ -49,32 +50,12 @@ client.Dispatcher.on(events.GATEWAY_READY, e => {
 	}
 	Constants.initializeDynamics(client, process);
 
-	let dataPuller = spawn('pwsh', [Constants.skarmRootPath + "pullData.ps1"]);
-    let skarmVersion = undefined;
 	Encrypt.initialize();
-	dataPuller.on('exit', (code) => {
-		console.log("Pulled in skarmData.\nGit revision count: " + code);
-        skarmVersion ??= code;
-        console.log("Using version data: ", skarmVersion);
-		// Users.initialize(client);
-		// Guilds.initialize(client);
-		Skarm.log("Connected as " + client.User.username + ". Yippee!\n");
-		main();
-	});
-	dataPuller.stderr.on("data", (err) => {
-		console.error(err.toString());
-	});
-	dataPuller.stdout.on("data", (message) => {
-		message = message.toString();
-		if (message.length > 1) {	//if this minimum isn't included, extra new lines get added between each printed line
-			console.log(message.replaceAll("\n", "").replaceAll("\r", ""));
-		}
-        
-        let versionStartString = "Version: ";
-        if(message.includes(versionStartString)){
-            skarmVersion = message.substring(message.indexOf(versionStartString) + versionStartString.length);
-        }
-	});
+	let s = new ShantyCollection(); // needs to be done for guilds to load
+	Users.initialize(client);
+	await Guilds.initialize(client);
+	console.log("Connected as " + client.User.username + ". Yippee!\n");
+	main();
 });
 
 
@@ -88,7 +69,7 @@ client.Dispatcher.on(events.DISCONNECTED, e => {
 /**
  * Enter one-off task instructions here
  */
-async function main(){
+async function main() {
     // clear this random example
     let messages = await client.Channels.get("asdf").fetchMessages(10);
     let reactors = await msg.fetchReactions(emoji, client);
@@ -97,4 +78,6 @@ async function main(){
     membersToPrune.map(async (member) => {
         await member.unassignRole(role);
     });
+
+	process.exit();
 }
